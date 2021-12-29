da1e2899533767fe05ffaaad2551d1eecef91d70
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2021-12-29 13:16:11 +0100

    improve cartpole

--- python/src/main_cartpole_v0.py
+++ python/src/main_cartpole_v0.py
@@ -1,6 +1,8 @@
 import gym
 import time
 
+# Adapted from https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+
 env = gym.make('CartPole-v0')
 
 c_default_delay = 0.05
@@ -12,9 +14,16 @@ obs = env.reset()
 for _ in range(1000):
 
     n_step = env.action_space.sample()
-    env.step(n_step)
+
+    obs, reward, done, info = env.step(n_step)
+
+    # print(reward)
 
     env.render()
+
+    if done:
+        print("WARN", "Reset Env")
+        env.reset()
 
     time.sleep(c_default_delay)
 
